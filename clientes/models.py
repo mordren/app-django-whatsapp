abd0ca992aad2322f4340ac5505eb318a8efee3c
unknown <pc-roh.joaokko@gmail.com>
2022-10-28 16:46:15 -0300

    adicionando model

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from phone_field import PhoneField
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=60)
+    phone = PhoneField(blank=True, help_text='Contact Phone')
+    email = models.EmailField(blank=True, null=True)
+    
+    
+
+    #relação de um para um quando cada um objeto é ligado ao outro e não poderá ser ligado para outro.    
+        
--- gestor_laudo/urls.py
+++ gestor_laudo/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from clientes import urls as clientes_urls
 
-
 urlpatterns = [    
-    path('clientes/', include(clientes_urls)),
     path('admin/', admin.site.urls),
+    path('clientes/', include(clientes_urls)),
 ]
