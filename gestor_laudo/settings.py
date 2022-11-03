ea89a6552957c350f704c76966af14533b211191
mordren <joaokko@gmail.com>
2022-11-02 23:49:18 -0300

    tudo certo

--- gestor_laudo/settings.py
+++ gestor_laudo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#+g2_f0zkwr%!baj8ep)72of=24fz@*nfv_)!%*^!=#6j#38zc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['app-agil.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
