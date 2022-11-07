4a5dd40210c8cfc40f2b74b23418acff069e3a07
mordren <joaokko@gmail.com>
2022-11-07 09:54:11 -0300

    update heroku chrome

--- gestor_laudo/scrapers.py
+++ gestor_laudo/scrapers.py
@@ -20,8 +20,8 @@ def startHeroku():
     chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    print(chrome_options.chrome.chromedriverVersion)
+    chrome_options.add_argument("--no-sandbox")    
+    chrome_options.add_argument("user-agent=User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.62 Safari/537.36")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)              
     print('start service')     
     return driver
